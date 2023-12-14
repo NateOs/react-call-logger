7449f62747eb578e149ccfdd0102e98da1db12da
nateos <sodjanathan@gmail.com>
2023-12-14 20:44:12 +0000

    chore: working tailwind and chakra setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// App.jsx
+import TabsMenu from "./components/TabsMenu";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <div className="m-10">
+      <TabsMenu></TabsMenu>
+    </div>
   );
 }
 
---
+++ src/components/TabsMenu.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+
+function TabsMenu() {
+  return (
+    <div>
+      <Tabs>
+        <TabList>
+          <Tab>One</Tab>
+          <Tab>Two</Tab>
+          <Tab>Three</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  );
+}
+
+export default TabsMenu;
