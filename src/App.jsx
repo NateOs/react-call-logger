d85ee9757538da108fbaa47588bd73e7e7e70e48
Sodja Nathan  O. <sodjanathan@gmail.com>
2023-12-16 23:24:19 +0000

    Revert "chore: working tailwind and chakra setup"
    This reverts commit 7449f62747eb578e149ccfdd0102e98da1db12da.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-// App.jsx
-import Homepage from "./pages/Homepage";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  return <Homepage />;
+  const [count, setCount] = useState(0);
+
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+  );
 }
 
 export default App;
